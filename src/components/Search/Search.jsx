6057292551600c75243b6502c97af512f7b2365b
Valera Kamelkov <tahaburrent@gmail.com>
2023-04-23 02:08:54 +0200

    Update Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -25,7 +25,7 @@ export const Search = () => {
                 autoClose: 3000,
                 hideProgressBar: false,
                 closeOnClick: true,
-                pauseOnHover: true,
+                pauseOnHover: false,
                 draggable: true,
                 progress: undefined,
                 theme: 'colored',
